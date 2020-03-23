8b377eea5e213fed5593a53728df1f206aadb54c
Helge <elitaer@gmail.com>
2020-03-23 22:14:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import ListItem from './components/ListItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    items: [],
+    input: '',
+    val: 0
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  submitHandler = (e) => {
+    if (e.key === 'Enter') {
+
+      // const newList = [...this.state.items, 
+      //   {
+      //     name: this.state.input,
+      //     key: this.state.input + '-' + this.state.items.length,
+      //     clicked: false
+      //   }];
+      this.setState(prev => (
+        {
+          items: [
+            ...prev.items, 
+            {
+              name: prev.input,
+              key: prev.input + '-' + prev.items.length
+            }
+          ],
+          input: ''
+        }
+      ))
+    }
+  }
+
+  updateProgress = () => {
+    let cur = 0
+    this.state.items.forEach(e => {
+      if (e.clicked) cur++
+    })
+
+    this.setState({
+      val: cur/this.state.items.length * 100
+    })
+  }
+
+  clickHandler = (e) => {
+    this.setState(prev => (
+      {
+        items: prev.items.map(el => 
+          el.key === e.key ? {...el, clicked: e.clicked} : el
+        )
+      }
+    ), () => this.updateProgress())
+  }
+
+  clearList = () => {
+    this.setState({
+      items: [],
+      input: '',
+      val: 0,
+    })
+    localStorage.clear();
+  }
+
+  render() {
+    return (
+      <div>
+        <header className='App-header'>
+          <progress max='100' value={this.state.val}></progress>
+          <input id='todoinput' placeholder='insert todo' value={this.state.input} onChange={this.changeHandler} onKeyDown={this.submitHandler}></input>
+          {this.state.items.map(item => {
+            return(
+              <ListItem key={item.key} itemKey={item.key} msg={item.name} onChildClick={this.clickHandler} clicked={item.clicked} />
+            )
+          }
+          )}
+          <button onClick={this.clearList}>Clear List</button>
+        </header>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('todos')) { this.setState(
+      JSON.parse(localStorage.getItem('todos')) 
+    )}
+  }
+
+
+  componentDidUpdate() {
+    localStorage.setItem('todos', JSON.stringify(this.state))
+  }
 }
 
 export default App;
